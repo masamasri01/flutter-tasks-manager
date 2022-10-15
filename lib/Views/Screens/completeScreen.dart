147bbc72d542698455626c196fdd7dce3dbeb02e
Masa Hassan Masri <93089580+masamasri01@users.noreply.github.com>
2022-10-15 13:59:37 +0300

    Add files via upload

---
+++ lib/Views/Screens/completeScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sat8_10/Views/widgets/TaskWidget.dart';
+import 'package:sat8_10/task_management/models/task_model.dart';
+
+class complete extends StatelessWidget {
+  Function function;
+  complete(this.function);
+
+  @override
+  Widget build(BuildContext context) {
+    TaskModel taskModel;
+    // TODO: implement build
+    return ListView.builder(
+      itemCount: tasks.where((element) => element.iscomplete).length,
+      itemBuilder: (context, index) {
+        return TaskWidget(
+            tasks.where((e) => e.iscomplete).toList()[index], function);
+      },
+    );
+    throw UnimplementedError();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'task_management/main_tasks_screen.dart';
+
+void main() {
+  runApp(MaterialApp(home: MainScreenView()));
+}
